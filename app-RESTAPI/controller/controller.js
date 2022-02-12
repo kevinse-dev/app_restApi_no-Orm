140784890a6ef2410807e9ee69d46c85c0018770
kavinSebastian <kavin.sebastian25@gmail.com>
2022-02-12 18:01:11 +0700

    show all mahasiswa

--- app-RESTAPI/controller/controller.js
+++ app-RESTAPI/controller/controller.js
@@ -6,3 +6,12 @@ const connect = require('../connect_db')
 exports.index = (req, res) => {
     response.success(res, 'mysql connected!')
 }
+
+exports.findAll = (req, res) => {
+    const querySql = `SELECT * FROM mahasiswa`
+
+    connect.query(querySql, (err, rows, fields) => {
+        if(err) {console.log('err:' , err)}
+        response.success(res, rows)
+    })
+}
--- app-RESTAPI/route/router.js
+++ app-RESTAPI/route/router.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     const controller = require('../controller/controller')
 
     app.route('/').get(controller.index)
+    app.route('/mahasiswa').get(controller.findAll)
 }
